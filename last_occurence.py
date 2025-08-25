d3b9f0fa9cfd4668feb8a38fbdd1cc337120f8e9
Archana  S M <51913719+archaunni@users.noreply.github.com>
2025-08-25 17:28:21 +0530

    Create last_occurrence.py

---
+++ last_occurence.py
@@ -0,0 +1,19 @@
+def last_index(arr, target):
+    len_arr = len(arr)
+    low, high = 0, len_arr - 1
+    set_index=-1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] == target:
+            set_index = mid
+            low = mid+1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return set_index
+
+arr=[1,2,2,2,3,3,4,4,4,4,5,6]
+target=4
+last= last_index(arr, target)
+print(last)
